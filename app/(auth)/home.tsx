c752a2661e6ebff8ede0465e59ce1a663b44dfce
owie <lucasb.empreendimentos@gmail.com>
2024-12-30 23:58:12 -0300

    Alterações novas

---
+++ app/(auth)/home.tsx
@@ -0,0 +1,24 @@
+import { Text, StyleSheet ,View } from "react-native";
+import { useUser } from "@clerk/clerk-expo"
+import { Link } from "expo-router";
+ 
+export default function Home(){
+    const { user } = useUser();
+
+
+    return(
+        <View>
+            <Text>Pagina Home</Text>
+            <Text>Email: {user?.emailAddresses[0].emailAddress}</Text>
+
+            <Link href='/profile'>
+            Meu perfil
+            </Link>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+})
+
+
